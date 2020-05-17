1dd791a2831e6bdbb06a0d704f13341cae2da168
hdnguyen2010 <hdnguyen2010@gmail.com>
2020-05-17 18:22:37 -0500

    landing page has a styling, information and an avatar. add grid and card to Projects

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import React from 'react';
 import './App.css';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
-import {BrowserRouter, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="demo-big-content">
-    <Layout>
-        <Header title="Title" scroll>
-            <Navigation>
-            <BrowserRouter>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </BrowserRouter>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <BrowserRouter>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </BrowserRouter>
-            </Navigation>
-        </Drawer>
-        <Content>
-          <Main />
-        </Content>
-    </Layout>
-  </div>
-  );
+    return (
+      <Router>
+        <div className="demo-big-content">
+          <Layout>
+              <Header className="header-color" title="Title" scroll>
+                  <Navigation>            
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>           
+                  </Navigation>          
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>            
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>            
+                  </Navigation>
+              </Drawer>       
+              <Content>
+                <Main />
+              </Content>
+          </Layout>
+      </div>
+    </Router>
+    );
 }
 
 export default App;
-
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,14 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
-
-class Landing extends Component{
-	render() {
-		return(
-			<div>
-				<h1>Landing Page</h1>
+class Landing extends Component {
+    render() {
+        return (
+            <div style={{width: '100%', margin: 'auto'}}>
+				<Grid className="landing-grid">
+					<Cell col={12}>
+						<img 
+							src="https://gao2la.ch.files.1drv.com/y4mKWziT-JPmXjxWiuroGttiUTJ1yizSKZ1vj2VljZQkb9K2Wipw25jV9hkdbekr63Pe81ReT7xonYsS0YY-SFxxZ2Q2bZKBQ08jiJ-QAvRkAd3LRRlYA1GRTsB2xQQd_4kGhi6yLWy4ntvSCLYzSxhLwB0u8cNBv8Vzer7vy10KThokweecdUYnrxyDfzaiQgw-ads17SQ-nkealrAHqU_mQ?width=718&height=744&cropmode=none"
+							atl="avatar"
+							className="avatar-img"
+						/>
+						<div className="banner-text">
+							<h1>Full Stack Web Developer</h1>
+							<hr/>
+							<p>JAVA | JavaScript | Bootstrap | HTML/CSS | React</p>
+							<div className="social-links">
+								{/*LinkedIn*/}
+								<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-linkedin-square" aria-hidden="true" />
+								</a>
+							{/*Github*/}
+								<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-github-square" aria-hidden="true" />
+								</a>
+							</div>
+						</div>
+					</Cell>
+				</Grid>
 			</div>
-		)
-	}
+        )
+    }
 }
 
 export default Landing;
