e26b76f485d8c85cca255fa0d280cb7606841b46
hdnguyen2010 <hdnguyen2010@gmail.com>
2020-05-17 20:00:26 -0500

    fixed up contact page and added styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -60,8 +60,52 @@ class Projects extends Component {
 	    }
 	    else if(this.state.activeTab === 1) {
 	    	return(
-	    		<div>
-	    			<h1>This is Angular</h1>
+	    		<div className="projects-grid">
+	    			{/* Project 1 */}
+	    			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+	    				<CardTitle style={{color: 'black', height: '176px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'}}>React Project #1</CardTitle>
+	    				<CardText>
+	    					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	    				</CardText>
+	    				<CardActions border>
+	    				<Button colored>GitHub</Button>
+	    				<Button colored>CodePen</Button>
+	    				<Button colored>LiveDemo</Button>
+	    				</CardActions>
+	    				<CardMenu style={{color: '#fff'}}>
+	    					<IconButton name='share'/>
+	    				</CardMenu>
+	    			</Card>
+	    			{/* Project 2 */}
+	    			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+	    				<CardTitle style={{color: 'black', height: '176px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'}}>React Project #1</CardTitle>
+	    				<CardText>
+	    					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	    				</CardText>
+	    				<CardActions border>
+	    				<Button colored>GitHub</Button>
+	    				<Button colored>CodePen</Button>
+	    				<Button colored>LiveDemo</Button>
+	    				</CardActions>
+	    				<CardMenu style={{color: '#fff'}}>
+	    					<IconButton name='share'/>
+	    				</CardMenu>
+	    			</Card>
+	    			{/* Project 3 */}
+	    			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+	    				<CardTitle style={{color: 'black', height: '176px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'}}>React Project #1</CardTitle>
+	    				<CardText>
+	    					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	    				</CardText>
+	    				<CardActions border>
+	    				<Button colored>GitHub</Button>
+	    				<Button colored>CodePen</Button>
+	    				<Button colored>LiveDemo</Button>
+	    				</CardActions>
+	    				<CardMenu style={{color: '#fff'}}>
+	    					<IconButton name='share'/>
+	    				</CardMenu>
+	    			</Card>
 	    		</div>
 	    	)
 	    }
