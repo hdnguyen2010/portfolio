a771fc92b82297d5c6e7b8edacbc4f79a553d061
hdnguyen2010 <hdnguyen2010@gmail.com>
2020-05-20 19:53:54 -0500

    updated project 1

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,6 +5,21 @@ class Projects extends Component {
     render() {
         return (
             <div className="projects-grid">
+	    			{/* Project 1 */}
+	    			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+	    				<CardTitle style={{color: 'black', height: '176px', background:'url(/images/vit-prev.png) center / cover'}}>Vitamin-DB</CardTitle>
+	    				<CardText>
+	    					An app that keeps track of user’s health history, eye prescription, immunization and doctor information
+	    				</CardText>
+	    				<CardActions border>
+	    				<Button href="https://www.youtube.com/watch?v=vEFQblKqqkI&feature=youtu.be" colored>Video</Button>
+	    				<Button href="https://github.com/hdnguyen2010/vitamin-db" colored>GitHub</Button>
+	    				<Button href="http://vitamin-db.herokuapp.com/" colored>LiveDemo</Button>
+	    				</CardActions>
+	    				<CardMenu style={{color: '#fff'}}>
+	    				</CardMenu>
+	    			</Card>
+	    			
 	    			{/* Project 2 */}
 	    			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
 	    				<CardTitle style={{color: 'black', height: '176px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'}}>React Project #2</CardTitle>
@@ -13,21 +28,6 @@ class Projects extends Component {
 	    				</p>
 	    				<CardActions border>
 	    				<Button colored>GitHub</Button>
-	    				<Button colored>LiveDemo</Button>
-	    				</CardActions>
-	    				<CardMenu style={{color: '#fff'}}>
-	    				</CardMenu>
-	    			</Card>
-	    			{/* Project 1 */}
-	    			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-	    				<CardTitle style={{color: 'white', height: '176px', background:'url(/images/vit-prev.png) center / cover'}}>Vitamin-DB</CardTitle>
-	    				<CardText>
-	    					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-	    				</CardText>
-	    				<CardActions border>
-	    				<Button href="https://www.youtube.com/watch?v=vEFQblKqqkI&feature=youtu.be" colored>Demo</Button>
-	    				<Button href="https://github.com/hdnguyen2010/vitamin-db" colored>GitHub</Button>
-	    				<Button href="http://vitamin-db.herokuapp.com/" colored>Website</Button>
 	    				</CardActions>
 	    				<CardMenu style={{color: '#fff'}}>
 	    				</CardMenu>
