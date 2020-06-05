065341473d6831f9739519b972bf54570d061314
hdnguyen2010 <hdnguyen2010@gmail.com>
2020-06-05 15:43:14 -0500

    updated banner and Links

--- src/App.js
+++ src/App.js
@@ -12,23 +12,44 @@ function App() {
           <Layout fixedDrawer>
               <Drawer 
               className="header-color"
-              // style={{width= }}
+              style={{width:"300px"}} 
               >
-            <h2>Huy Nguyen</h2>
+            <h2 className="full-name">Huy Nguyen</h2>
               <img
               alt="avatar"
-              style={{width: '100%', height: '25%'}}
+              style={{paddingLeft: '10%', width: '80%', height: '25%'}}
               src="/images/avatar_2.png"
             />
-                  <Navigation>
-                      <Link style={{color: 'black'}} to="/">Home</Link>
+            <div className="social-links">
+                {/*LinkedIn*/}
+                <a href="https://www.linkedin.com/in/hdnguyen2010" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+              {/*Github*/}
+                <a href="https://github.com/hdnguyen2010" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+              {/*Instagram*/}
+                <a href="https://www.instagram.com/HuyJonnyNguyen/" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-instagram" aria-hidden="true" />
+                </a>
+              {/*Facebook*/}
+                <a href="https://www.facebook.com/Jonny5Win/?viewas=100000686899395" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-facebook" aria-hidden="true" />
+                </a>
+              </div>
+              <div style={{float:'center', margin:'20px' }}>
+                <hr style={{width:'83%', borderTop:'1px solid grey', margin:'auto'}}/>
+                </div>
+                 <Navigation style={{paddingLeft:'10%', paddingTop:'0%'}}>
+                      <Link style={{color: 'black'}} to="/">About Me</Link>
                       <Link style={{color: 'black'}} to="/projects">Projects</Link> 
                       <Link style={{color: 'black'}} to="/contact">Contact</Link>            
                       <a style={{color: 'black', fontweight:'bold'}} href={Pdf} traget="_blank">Resume</a>          
-                  </Navigation>          
+                  </Navigation>
               </Drawer>
                   
-              <Content>
+              <Content style={{marginLeft:'320px'}}>
                 <Main />
               </Content>
           </Layout>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -4,37 +4,10 @@ import { Grid, Cell } from 'react-mdl';
 class Landing extends Component {
     render() {
         return (
-            <div>
-				<Grid className="landing-grid">
-					<Cell col={12}>
-						
-						<div className="banner-text">
-							<h1>Student</h1>
-							<h2>I am studying for a B.S. in Computer Game Development at Lamar University. I expect to graduate in Fall 2021.</h2>
-							<hr/>
-							<h2>Skills: JAVA | JavaScript | Bootstrap | HTML/CSS | React</h2>
-							<hr/>
-							<div className="social-links">
-								{/*LinkedIn*/}
-								<a href="https://www.linkedin.com/in/hdnguyen2010" rel="noopener noreferrer" target="_blank">
-									<i className="fa fa-linkedin-square" aria-hidden="true" />
-								</a>
-							{/*Github*/}
-								<a href="https://github.com/hdnguyen2010" rel="noopener noreferrer" target="_blank">
-									<i className="fa fa-github-square" aria-hidden="true" />
-								</a>
-							{/*Instagram*/}
-								<a href="https://www.instagram.com/jonnyboy8p/" rel="noopener noreferrer" target="_blank">
-									<i className="fa fa-instagram" aria-hidden="true" />
-								</a>
-							{/*Facebook*/}
-								<a href="https://www.facebook.com/Jonny5Win/?viewas=100000686899395" rel="noopener noreferrer" target="_blank">
-									<i className="fa fa-facebook" aria-hidden="true" />
-								</a>
-							</div>
-						</div>
-					</Cell>
-				</Grid>
+            <div className="banner-text">
+				<h1>Student</h1>
+				<h2>I am studying for a B.S. in Computer Game Development at Lamar University. I expect to graduate in Fall 2021.</h2>
+				<h2>Skills: JAVA | JavaScript | Bootstrap | HTML/CSS | React</h2>
 			</div>
         )
     }
