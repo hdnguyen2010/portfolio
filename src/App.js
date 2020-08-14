989737e7a7b72e56a96ecf847432706a8b906444
hdnguyen2010 <hdnguyen2010@gmail.com>
2020-08-13 22:31:14 -0500

    made particles be more interactive and cleaned some unused code

--- src/App.js
+++ src/App.js
@@ -8,30 +8,97 @@
 
     function App() {
         return (
-          <Router>            
+            <Router>            
             <div>
               <Particles
                 style={{ position: "absolute" }}
                 height="95%"
                 width="95%"
                 params={{
-                  particles: {
-                    color: {
-                      value: "#000000"
-                    },
-                    line_linked: {
-                      color: {
-                        value: "#000000"
-                      }
-                    },
-                    number: {
-                      value: 50
-                    },
-                    size: {
-                      value: 3
-                    }
-                  }
-                }}
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 800
+              }
+            },
+            color: {
+              value: "#000000"
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000"
+              },
+              image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100
+              }
+            },
+            opacity: {
+              value: 0.,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+              }
+            },
+            size: {
+              value: 3,
+              random: false,
+              anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0.1,
+                sync: false
+              }
+            },
+            line_linked: {
+              enable_auto: true,
+              distance: 100,
+              color: "#000000",
+              opacity: 1,
+              width: 1,
+              condensed_mode: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 600
+              }
+            },
+            move: {
+              enable: true,
+              speed: 1,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+              }
+            }
+          },
+          interactivity: {
+            detect_on: "window",
+            events: {
+              onhover: {
+                enable: true
+              },
+              onclick: {
+                enable: true
+              },
+              resize: true
+            }
+          },
+          retina_detect: true
+        }}
               />
               <Layout fixedDrawer>
                   <Drawer 
