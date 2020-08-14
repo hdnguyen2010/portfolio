2125bfeeca3adfcc1151e73869f05be157866225
hdnguyen2010 <hdnguyen2010@gmail.com>
2020-08-14 12:19:38 -0500

    Updates

---
+++ precache-manifest.f9cfa1c29573defd39de554f0dec6f98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6b3465786ff43512a76a9f7386bf75",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "ba95628339182d9c9430",
+    "url": "/my-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "9fd2df35598fff968a47",
+    "url": "/my-portfolio/static/css/main.188457c5.chunk.css"
+  },
+  {
+    "revision": "ba95628339182d9c9430",
+    "url": "/my-portfolio/static/js/2.d23b9226.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/my-portfolio/static/js/2.d23b9226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd2df35598fff968a47",
+    "url": "/my-portfolio/static/js/main.9fb13425.chunk.js"
+  },
+  {
+    "revision": "71f8fe099066da5003c6",
+    "url": "/my-portfolio/static/js/runtime-main.f6d94e01.js"
+  },
+  {
+    "revision": "185d7c10aad12a5647bcd80091d41ada",
+    "url": "/my-portfolio/static/media/Resume.185d7c10.pdf"
+  }
+]);
