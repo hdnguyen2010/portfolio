d62c8cf06b912478333e2523f9f091cb5b03c44e
hdnguyen2010 <hdnguyen2010@gmail.com>
2020-08-14 20:50:54 -0500

    fixed spelling error

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,7 +8,7 @@ class Landing extends Component {
 
 				<h2 style= {{fontFamily: "Lato", color:"grey"}}>Full-time Student</h2>
 
-				<h3 style= {{fontFamily: "Lato"}}>I am a Computer Science Game Development student at Lamar University and a future front-end developer in Texas. I have completed few projects with a team of four or more. My other interests also included techs, video games, and exercising and also being dedicated to growing as a programmer and as an individual,  </h3>
+				<h3 style= {{fontFamily: "Lato"}}>I am a Computer Science Game Development student at Lamar University and a future front-end developer in Texas. I have completed few projects with a team of four or more. My other interests also included techs, video games, and exercising and also being dedicated to growing as a programmer and as an individual.  </h3>
 
 				<h2 className= "copyright">Copyright © Huy 'Jonny' Nguyen 2020</h2>
 			</div>
