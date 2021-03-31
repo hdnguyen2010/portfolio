1c5d303bd24e93d97d485ab68387e0ee4bba5e53
hdnguyen2010 <hdnguyen2010@gmail.com>
2021-03-31 17:45:54 -0500

    fix refresh issue when page is live

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import { HashRouter } from 'react-router-dom'
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <HashRouter>
+  <App/>
+</HashRouter>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
