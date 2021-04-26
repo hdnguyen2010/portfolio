700930d484413a67c5f415f249cf8f320d49700b
hdnguyen2010 <hdnguyen2010@gmail.com>
2021-04-26 01:41:19 -0500

    adjust margin and added padding

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -4,13 +4,13 @@ class Landing extends Component {
     render() {
         return (
             <div className= "banner-text">
-				<h1 style= {{fontWeight: "bold", fontFamily: "Lato"}}>Hi, I am Huy 'Jonny' Nguyen</h1>
+				<h1 style= {{fontWeight: "bold", fontFamily: "Lato", paddingTop: "60px", backgroundColor: "white"}}>Hi, my name is Huy 'Jonny' Nguyen</h1>
 
-				<h2 style= {{fontFamily: "Lato", color:"grey"}}>Full-time Student</h2>
+				<h2 style= {{fontFamily: "Lato", color:"grey"}}>Building things for the web.</h2>
 
-				<h3 style= {{fontFamily: "Lato"}}>I am a Computer Science Game Development student at Lamar University and a future front-end developer in Texas. I have completed few projects with a team of four or more. My other interests also included techs, video games, and exercising and also being dedicated to growing as a programmer and as an individual.  </h3>
+				<p style= {{fontFamily: "Lato", fontSize: "25px", paddingTop: "60px", width: "95%",backgroundColor: "white"}}>I am a student at Lamar University majoring in Computer Science Game Development. My expected graduation date is December 2021. My interest in web development started back in 2016 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
 
-				<h2 className= "copyright">Copyright © Huy 'Jonny' Nguyen 2020</h2>
+				<p className= "copyright">Designed & Built by Huy 'Jonny' Nguyen</p>
 			</div>
         )
     }
